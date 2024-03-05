f0137dea37c9ca0d6291b9372465fe29b3a4a0ea
Lime <2650684600@qq.com>
2024-03-05 22:44:05 +0800

    无

--- Module/Comments.php
+++ Module/Comments.php
@@ -248,3 +248,106 @@ function preprocess_comment_content($commentdata)
 
 // 将preprocess_comment_content函数连接到preprocess_comment钩子
 add_filter('preprocess_comment', 'preprocess_comment_content');
+
+
+function Len_Comments_Js_Module()
+{
+?>
+    <script>
+        var OwO_demo = new OwO({
+            logo: 'OωO表情',
+            container: document.getElementsByClassName('OwO')[0],
+            target: document.getElementsByClassName('OwO-textarea')[0],
+            api: '<?php echo esc_url(get_template_directory_uri()); ?>/Assets/Lne-JavaScript/123.json',
+            position: 'down',
+            width: '50%',
+            maxHeight: '250px'
+        });
+        jQuery(document).ready(function($) {
+            var initialFormLocation = $('#commentform').parent();
+            var replyTargetId = null;
+
+            // 提交评论
+            $('#commentform').submit(function(event) {
+                event.preventDefault();
+
+                var formData = $(this).serialize();
+
+                $.ajax({
+                    url: '/wp-comments-post.php',
+                    method: 'POST',
+                    data: formData,
+                    dataType: 'html',
+                    success: function(response) {
+                        $('#commentform')[0].reset();
+                        $('#comments').load(location.href + ' #comments');
+
+                        // 如果存在回复目标 ID，则将表单放回原始位置
+                        if (replyTargetId !== null) {
+                            $('#commentform').detach().appendTo(initialFormLocation);
+                            replyTargetId = null;
+                        }
+                    },
+                    error: function(error) {
+                        console.error('Error:', error);
+                    },
+                });
+            });
+            // 回复评论
+            $('.comment-reply-link').click(function(event) {
+                event.preventDefault();
+
+                var parentCommentId = $(this).data('commentid');
+
+                // 更新回复链接的参数
+                var replyLink = $(this).attr('href');
+                replyLink += (replyLink.indexOf('?') !== -1 ? '&' : '?') + 'replytocom=' + parentCommentId + '#respond';
+                $(this).attr('href', replyLink);
+
+                // 如果用户再次点击相同的回复链接，将表单放回原始位置
+                if (replyTargetId === parentCommentId) {
+                    $('#commentform').detach().appendTo(initialFormLocation);
+                    replyTargetId = null;
+                } else {
+                    // 否则，将表单移到新的回复目标位置
+                    $('#commentform').detach().appendTo('#comment-' + parentCommentId);
+                    replyTargetId = parentCommentId;
+                }
+
+                $('#comment_parent').val(parentCommentId);
+            });
+        });
+        jQuery(function($) {
+            $('.comment_loadmore').click(function() {
+                var button = $(this);
+                // 减少当前评论页面的值
+                cpage--;
+                $.ajax({
+                    url: ajaxurl,
+                    data: {
+                        'action': 'cloadmore',
+                        'post_id': parent_post_id, // 当前文章
+                        'cpage': cpage, // 当前评论页
+                    },
+                    type: 'POST',
+                    beforeSend: function(xhr) {
+                        button.text('加载中...');
+                    },
+                    success: function(data) {
+                        if (data) {
+                            $('ol.len-comments-ol-block').append(data);
+                            button.text('加载更多');
+                            // 如果最后一页，则删除按钮
+                            if (cpage == 1)
+                                button.remove();
+                        } else {
+                            button.remove();
+                        }
+                    }
+                });
+                return false;
+            });
+        });
+    </script>
+<?php
+}
