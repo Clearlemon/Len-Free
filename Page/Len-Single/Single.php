5165cac7e29fe3dc9a4a8232747942a037ea5c34
大绵羊 <dmy@yj.dmygzs.cn>
2024-03-11 22:42:07 +0800

    1

--- Page/Len-Single/Single.php
+++ Page/Len-Single/Single.php
@@ -2,7 +2,17 @@
 
 <body>
     <img class="bady-background-block" src="/wp-content/themes/Len-Free/Assets/Len-Images/body-background.jpg" alt="">
-    
+    <script> 
+        const ap = new APlayer({
+            container: document.getElementById('aplayer'),
+            audio: [{
+                name: 'name',
+                artist: 'artist',
+                url: 'url.mp3',
+                cover: 'cover.jpg'
+            }]
+        });
+        </script>
     <main class="len-body-main len-body-m">
         <?php
         /**
@@ -53,7 +63,7 @@
                                         </div>
                                         <?php Len_Module_Switcher(false, false, true); ?>
                                     </div>
-                                    <div id="aplayer" class=""></div>
+                                    <div id="aplayer"></div>
                                 </div>
                             </div>
                     <?php
